4b6a0d23f38f95ee0043a14746bc682076e4a626
tekutokiari <sqcirc@yahoo.com>
2021-04-22 17:44:10 +0300

    as

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import config
 
 intents = discord.Intents.all()
 bot = commands.Bot(case_insensitive=True,intents=intents,command_prefix=config.prefix,help_command=None)
-for filename in os.listdir('./cogs'):
+for filename in os.listdir('cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
 
